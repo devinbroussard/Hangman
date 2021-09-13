afb0a1e89bf8d130e806e87c8f5cabb8c2d23650
Devin Broussard <devin.ray.broussard@gmail.com>
2021-09-13 12:58:19 -0500

    Created main game loops and hangman art

--- Hangman/Hangman/Game.cs
+++ Hangman/Hangman/Game.cs
@@ -6,8 +6,114 @@ namespace Hangman
 {
     class Game
     {
+        string[] hangmanArt = new string[] {
+            "****************************" +
+            "\n  +---+" +
+            "\n  |   |" +
+            "\n      |" +
+            "\n      |" +
+            "\n      |" +
+            "\n      |" +
+            "\n=========",
+            "  +---+" +
+            "\n   |   |" +
+            "\n   O   |" +
+            "\n       |" +
+            "\n       |" +
+            "\n       |" +
+            "\n=========",
+            "  +---+" +
+            "\n   |   |" +
+            "\n   O   |" +
+            "\n   |   |" +
+            "\n       |" +
+            "\n       |" +
+            "\n=========",
+            "  +---+" +
+            "\n   |   |" +
+            "\n   O   |" +
+            "\n  /|   |" +
+            "\n       |" +
+            "\n       |" +
+            "\n=========",
+            "  +---+" +
+            "\n   |   |" +
+            "\n   O   |" +
+            @"\n  /|\  |" +
+            "\n       |" +
+            "\n       |" +
+            "\n=========",
+            "\n   |   |" +
+            "\n   O   |" +
+            @"\n  /|\  |" +
+            "\n  /    |" +
+            "\n       |" +
+            "\n=========",
+            "\n   |   |" +
+            "\n   O   |" +
+            @"\n  /|\  |" +
+            @"\n  / \  |" +
+            "\n       |" +
+            "\n=========", };
+        int currentScene;
+        int hangmanIndex;
+        bool gameOver = false;
+
         public void Run()
         {
+            Start();
+
+            while (!gameOver)
+            {
+            Update();
+            }
+
+            End();
+        }
+
+        void Display()
+        {
+            switch (currentScene)
+            {
+                case 0:
+                    DisplayMainMenu();
+                    break;
+                case 1:
+                    RenderHangman();
+                    break;
+
+            }
+        }
+
+        void Update()
+        {
+            Display();
+        }
+
+        void Start()
+        {
+            currentScene = 0;
+            hangmanIndex = 0;
+        }
+
+        void End()
+        {
+
+        }
+
+        void DisplayMainMenu()
+        {
+            Console.WriteLine("Hello, this is a console-based Hangman game.");
+            Console.WriteLine("I hope you enjoy.");
+            Console.ReadKey(true);
+            Console.Clear();
+            currentScene++;
+        }
+
+        void RenderHangman()
+        {
+            Console.WriteLine(hangmanArt[hangmanIndex]);
+            Console.ReadKey(true);
 
         }
     }
